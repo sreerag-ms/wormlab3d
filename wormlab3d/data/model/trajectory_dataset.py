13276f3bbceecdb3ed42d00738be3a940404a4cd
tom0 <>
2021-03-17 10:58:35 +0000

    Dataset builder (initial port) trajectories, tags

---
+++ wormlab3d/data/model/trajectory_dataset.py
@@ -0,0 +1,33 @@
+from mongoengine import *
+
+from wormlab3d.data.model.tag import Tag
+from wormlab3d.data.model.trajectory import Trajectory
+
+DATA_TYPES = ['xyz', 'xyz_inv', 'bishop', 'cpca']
+
+
+class TrajectoryDataset(Document):
+    data_type = StringField(required=True, choices=DATA_TYPES)
+    train_test_split_target = FloatField(required=True, default=None, min_value=0, max_value=1)
+    train_test_split_actual = FloatField(required=True, default=None, min_value=0, max_value=1)
+    n_frames = IntField(required=True)
+    frame_shift = IntField(required=True)
+    n_cpca_components = IntField()
+    restrict_tags = ListField(ReferenceField(Tag))
+    restrict_concs = ListField(FloatField())
+    restrict_length = ListField(IntField())
+    include_mirrors = BooleanField(required=True, default=False)
+    inv_opt_params = DictField()
+    trajectories_train = ListField(ReferenceField[Trajectory])
+    trajectories_test = ListField(ReferenceField[Trajectory])
+    size_all = IntField(default=0)
+    size_train = IntField(default=0)
+    size_test = IntField(default=0)
+
+    def set_trajectories(self, train, test):
+        self.trajectories_train = train
+        self.trajectories_test = test
+        self.size_all = len(train) + len(test)
+        self.size_train = len(train)
+        self.size_test = len(test)
+        self.train_test_split_actual = len(train) / (len(train) + len(test))
--- wormlab3d/data/model/trial.py
+++ wormlab3d/data/model/trial.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from mongoengine import *
 
 from wormlab3d.data.model.experiment import Experiment
@@ -23,3 +25,35 @@ class Trial(Document):
             trial=self,
             frame_num=frame_num
         )
+
+    def get_frames(self, filter=None) -> List[Frame]:
+        if filter is None:
+            filter = {}
+        return Frame.objects.get(
+            trial=self,
+            **filter
+        )
+
+    def get_clips(self, filter: Dict=None) -> List[List[Frame]]:
+        """
+        Return a list of clips (frame-sequences) where frames in each clip match the given filter.
+        Ensures that the tags are consistent through the clip.
+        If no filter is provided this should just return a single clip.
+        """
+        frames = self.get_frames(filter)
+        clips = []
+        clip = []
+        prev_num = -1
+        prev_tags = []
+        for f in frames:
+            if f.frame_num == prev_num + 1 and f.tags == prev_tags:
+                clip.append(f)
+                prev_num += 1
+            else:
+                if len(clip):
+                    clips.append(clip)
+                clip = [f]
+                prev_num = f.frame_num
+                prev_tags = f.tags
+
+        return clips
