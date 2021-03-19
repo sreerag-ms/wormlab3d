7dce2eeab21f7ca3197c9d62c3f94c16a45c27f5
tom0 <>
2021-03-19 18:24:54 +0000

    Contour test

--- tests/preprocessing/test_contours.py
+++ tests/preprocessing/test_contours.py
@@ -1,23 +1,21 @@
 from wormlab3d import ROOT_PATH, logger
-from wormlab3d.preprocessing.contour import contour_centre, VideoContour
-#from wormlab3d.preprocessing.img_util import my_imshow
+from wormlab3d.preprocessing.contour import contour_centre
+# from wormlab3d.preprocessing.img_util import my_imshow
+from wormlab3d.preprocessing.video_reader import VideoReader
 
 TEST_DATA_PATH = ROOT_PATH + '/data/test-data'
 
 
 def test_find_contours_in_video():
-	video_path = TEST_DATA_PATH + '/Cam1_firstsec.avi'
-	bg_path = TEST_DATA_PATH + '/background-images/Cam1_firstsec.png'
-	vidcap = VideoContour(video_path, bg_path)
+    video_path = TEST_DATA_PATH + '/Cam1_firstsec.avi'
+    bg_path = TEST_DATA_PATH + '/background-images/Cam1_firstsec.png'
 
-	success, image, contours = vidcap.read()
-	while success:
-		for c in contours:
-			logger.info(f'center = {contour_centre(c)}')
-			# my_imshow(f, center(c))
-		success, image, contours = vidcap.read()
+    video = VideoReader(video_path, bg_path)
+    for _ in video:
+        contours = video.find_contours()
+        for c in contours:
+            logger.info(f'frame #{video.current_frame}, center = {contour_centre(c)}')
 
 
 if __name__ == '__main__':
-	test_find_contours_in_video()
-	# test_triangulate_2d_video()
+    test_find_contours_in_video()
