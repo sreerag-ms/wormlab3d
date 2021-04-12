466907379dc58a5282701152843052949c45add0
tom0 <>
2021-04-12 16:14:33 +0100

    Video triplet reader - fixes to catch unreadable videos and keep returning from any that are readable

--- wormlab3d/preprocessing/video_triplet_reader.py
+++ wormlab3d/preprocessing/video_triplet_reader.py
@@ -1,9 +1,10 @@
+from collections import OrderedDict
 from typing import List
 
 import numpy as np
 import pims
 
-from wormlab3d import CAMERA_IDXS
+from wormlab3d import CAMERA_IDXS, logger
 from wormlab3d.preprocessing.video_reader import VideoReader
 
 
@@ -48,35 +49,50 @@ class VideoTripletReader:
     def __getitem__(self, idx: int) -> List[pims.Frame]:
         return [self.readers[c][idx] for c in CAMERA_IDXS]
 
+    def get_frame_counts(self) -> List[int]:
+        return [len(self.readers[c]) for c in CAMERA_IDXS]
+
     def set_frame_num(self, idx: int):
-        assert 0 <= idx <= len(self.readers[0])
+        """Set the frame number for all readers. (Skips assertion checks.)"""
         self.current_frame = idx
         for r in self.readers:
             r.current_frame = idx
 
-    def get_images(self, invert: bool = False, subtract_background: bool = False) -> List[pims.Frame]:
-        return [
-            self.readers[c].get_image(
-                invert=invert,
-                subtract_background=subtract_background
-            )
-            for c in CAMERA_IDXS
-        ]
-
-    def find_contours(self, subtract_background: bool = True, cont_threshold: float = None) -> List[List[np.ndarray]]:
-        contours = []
+    def get_images(self, invert: bool = False, subtract_background: bool = False) -> OrderedDict[int, pims.Frame]:
+        images = OrderedDict()
         for c in CAMERA_IDXS:
-            contours.append(
-                self.readers[c].find_contours(subtract_background=subtract_background, cont_threshold=cont_threshold)
-            )
+            try:
+                img = self.readers[c].get_image(
+                    invert=invert,
+                    subtract_background=subtract_background
+                )
+                images[c] = img
+            except (IndexError, AssertionError) as e:
+                logger.warning(f'Error reading image from camera {c}: {e}')
+        return images
+
+    def find_contours(self, subtract_background: bool = True, cont_threshold: float = None) \
+            -> OrderedDict[int, List[np.ndarray]]:
+        contours = OrderedDict()
+        for c in CAMERA_IDXS:
+            try:
+                conts = self.readers[c].find_contours(
+                    subtract_background=subtract_background,
+                    cont_threshold=cont_threshold
+                )
+                contours[c] = conts
+            except (IndexError, AssertionError) as e:
+                logger.warning(f'Error finding contours in camera {c}: {e}')
         return contours
 
-    def find_objects(self, cont_threshold: float = None) -> List[list]:
-        centres = []
+    def find_objects(self, cont_threshold: float = None) -> OrderedDict[int, list]:
+        centres = OrderedDict()
         for c in CAMERA_IDXS:
-            centres.append(
-                self.readers[c].find_objects(cont_threshold=cont_threshold)
-            )
+            try:
+                objs = self.readers[c].find_objects(cont_threshold=cont_threshold)
+                centres[c] = objs
+            except (IndexError, AssertionError) as e:
+                logger.warning(f'Error finding objects in camera {c}: {e}')
         return centres
 
     def close(self):
