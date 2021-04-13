3ae551320fcb81da3bda5731dcdfcbdd419dd594
tom0 <>
2021-04-13 11:57:31 +0100

    Check frames script, add brightness checks

--- scripts/preprocessing/check_frames.py
+++ scripts/preprocessing/check_frames.py
@@ -1,4 +1,5 @@
 import gc
+from collections import OrderedDict
 from subprocess import CalledProcessError
 
 import cv2
@@ -14,30 +15,34 @@ def check_frames(
         experiment_id: int = None,
         trial_id: int = None,
         camera_idx: int = None,
-        check_frame_counts: bool = True,
-        check_backgrounds: bool = True,
+        check_frame_counts: bool = False,
+        check_backgrounds: bool = False,
+        check_brightnesses: bool = True,
         check_centres: bool = True,
-        check_images: bool = True
+        check_images: bool = True,
 ):
     """
-    Runs a sweep of the trials and frames in the database and checks backgrounds, centres and images.
+    Runs a sweep of the trials and frames in the database and checks backgrounds, centres, images and brightness levels.
     """
-    assert any([check_frame_counts, check_backgrounds, check_centres, check_images])
+    assert any([check_frame_counts, check_backgrounds, check_brightnesses, check_centres, check_images])
     trials, cam_idxs = resolve_targets(experiment_id, trial_id, camera_idx)
     trial_ids = [t.id for t in trials]
 
     frame_counts = {}
-    trials_incorrect_counts = {}
+    trials_incorrect_counts = OrderedDict()
     trials_missing_backgrounds = []
-    trials_missing_centres_2d = {}
-    trials_missing_centres_3d = {}
-    trials_missing_images = {}
+    trials_missing_centres_2d = OrderedDict()
+    trials_missing_centres_3d = OrderedDict()
+    trials_missing_images = OrderedDict()
+    trials_missing_brightnesses = OrderedDict()
 
     # Iterate over matching trials
     for trial_id in trial_ids:
         logger.info(f'Checking trial id={trial_id}.')
         trial = Trial.objects.get(id=trial_id)
-        frame_counts[trial.id] = trial.n_frames
+        if len(trial.n_frames) == 0:
+            trial.n_frames = 0, 0, 0
+        frame_counts[trial_id] = list(trial.n_frames)
 
         # Check frame counts
         if check_frame_counts:
@@ -70,6 +75,21 @@ def check_frames(
                         bgs_ok[c] = True
             if not all(bgs_ok.values()):
                 trials_missing_backgrounds.append(trial.id)
+
+        # Check for pre-computed max-brightness values
+        if check_brightnesses:
+            filters = {
+                '__raw__': {
+                    '$or': [
+                        {'max_brightnesses': None},
+                        {'max_brightnesses': {'$size': 0}},
+                    ]
+                },
+                'frame_num__lt': max(trial.n_frames),
+            }
+            n_frames_missing_brightnesses = trial.get_frames(filters).count()
+            if n_frames_missing_brightnesses > 0:
+                trials_missing_brightnesses[trial.id] = n_frames_missing_brightnesses
 
         if check_centres:
             # 2D centres - check present in each camera
@@ -130,9 +150,11 @@ def check_frames(
     if check_backgrounds:
         log.append('------------- BACKGROUNDS -------------')
         if len(trials_missing_backgrounds) == 0:
-            log.append('All present and correct!')
+            log.append('All present and readable!')
         else:
-            log.append(f'{len(trials_missing_backgrounds)} trials with missing or unreadable backgrounds:')
+            trials_missing_backgrounds = OrderedDict(sorted(trials_missing_backgrounds.items()))
+            log.append(
+                f'{len(trials_missing_backgrounds)}/{len(trial_ids)} trials with missing or unreadable backgrounds:')
             log.append(trials_missing_backgrounds)
         log.append('\n\n')
 
@@ -141,8 +163,9 @@ def check_frames(
         if len(trials_incorrect_counts) == 0:
             log.append('All match up!')
         else:
-            log.append(f'{len(trials_incorrect_counts)} trials with inconsistent frame counts!')
+            log.append(f'{len(trials_incorrect_counts)}/{len(trial_ids)} trials with inconsistent frame counts!')
             log.append('id: \t database:cam0/cam1/cam2 \t videos:cam0/cam1/cam2 \t %/%/%')
+
             for trial_id, counts in trials_incorrect_counts.items():
                 log.append(
                     f'{trial_id}'
@@ -153,45 +176,30 @@ def check_frames(
                     f' / {counts[2] / frame_counts[trial_id][0] * 100:.2f}%)')
         log.append('\n\n')
 
+    def make_log_section(title, missing_counts, n_frames_minmax):
+        log.append(f'------------- {title} -------------')
+        if len(missing_counts) == 0:
+            log.append('All present in database!')
+        else:
+            missing_counts = OrderedDict(sorted(missing_counts.items(), key=lambda item: -item[1]))
+            log.append(f'{len(missing_counts)}/{len(trial_ids)} trials with missing {title}:')
+            log.append('id: \t missing/total \t %')
+            minmax = min if n_frames_minmax == 'min' else max
+            for trial_id, count in missing_counts.items():
+                n_trial_frames = minmax(frame_counts[trial_id])
+                log.append(
+                    f'{trial_id}'
+                    f'\t {count}/{n_trial_frames} '
+                    f'\t ({count / n_trial_frames * 100:.2f}%)')
+        log.append('\n\n')
+
+    if check_brightnesses:
+        make_log_section('MAX BRIGHTNESSES', trials_missing_brightnesses, 'max')
     if check_centres:
-        log.append('------------- CENTRES 2D -------------')
-        if len(trials_missing_centres_2d) == 0:
-            log.append('All present and correct!')
-        else:
-            log.append(f'{len(trials_missing_centres_2d)} trials with missing 2D centres:')
-            log.append('id: \t missing/total \t %')
-            for trial_id, count in trials_missing_centres_2d.items():
-                log.append(
-                    f'{trial_id}'
-                    f'\t {count}/{max(frame_counts[trial_id])} '
-                    f'\t ({count / max(frame_counts[trial_id]) * 100:.2f}%)')
-        log.append('\n\n')
-
-        log.append('------------- CENTRES 3D -------------')
-        if len(trials_missing_centres_3d) == 0:
-            log.append('All present and correct!')
-        else:
-            log.append(f'{len(trials_missing_centres_3d)} trials with missing 3D centres:')
-            log.append('id: \t missing/total \t %')
-            for trial_id, count in trials_missing_centres_3d.items():
-                log.append(
-                    f'{trial_id}'
-                    f'\t {count}/{min(frame_counts[trial_id])} '
-                    f'\t ({count / min(frame_counts[trial_id]) * 100:.2f}%)')
-        log.append('\n\n')
-
+        make_log_section('CENTRES 2D', trials_missing_centres_2d, 'max')
+        make_log_section('CENTRES 3D', trials_missing_centres_3d, 'min')
     if check_images:
-        log.append('------------- PREPARED IMAGES -------------')
-        if len(trials_missing_images) == 0:
-            log.append('All present and correct!')
-        else:
-            log.append(f'{len(trials_missing_images)} trials with missing prepared images:')
-            log.append('id: \t missing/total \t %')
-            for trial_id, count in trials_missing_images.items():
-                log.append(
-                    f'{trial_id}'
-                    f'\t {count}/{min(frame_counts[trial_id])} '
-                    f'\t ({count / min(frame_counts[trial_id]) * 100:.2f}%)')
+        make_log_section('PREPARED IMAGES', trials_missing_images, 'min')
 
     logger.info('\n'.join(log))
 
