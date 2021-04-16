82a93f11a8921bb0407cab376554e6fdb8c559f5
tom0 <>
2021-04-16 15:29:19 +0100

    Generate midline2d dataset - reapply fixes

--- wormlab3d/midlines2d/generate_midline2d_dataset.py
+++ wormlab3d/midlines2d/generate_midline2d_dataset.py
@@ -2,7 +2,7 @@ from bson import ObjectId
 
 from wormlab3d import logger
 from wormlab3d.data.model import Midline2D
-from wormlab3d.data.model.dataset import DatasetMidline2D, Dataset
+from wormlab3d.data.model.dataset import DatasetMidline2D
 from wormlab3d.midlines2d.args import DatasetMidline2DArgs
 from wormlab3d.nn.generate_dataset import build_pipeline, build_dataset
 
@@ -24,18 +24,39 @@ def generate_midline2d_dataset(args: DatasetMidline2DArgs, fix_frames: bool = Fa
         logger.debug(f'Checking frame id={frame.id}')
         try:
             if fix_frames:
+                ok = True
                 if not frame.centres_2d_available():
                     logger.warning('Frame does not have 2d centre points available for all views, generating now.')
-                    frame.generate_centres_2d()
-                    frame.save()
-                if frame.centre_3d is None:
+                    centres_2d, centres_2d_thresholds = frame.generate_centres_2d()
+                    frame.update_centres_2d(centres_2d, centres_2d_thresholds)
+                    if frame.centres_2d_available():
+                        frame.save()
+                    else:
+                        ok = False
+                    frame.reload()
+
+                if ok and frame.centre_3d is None:
                     logger.warning('Frame does not have 3d centre point available, generating now.')
-                    frame.generate_centre_3d()
-                    frame.save()
-                if len(frame.images) != 3:
+                    res = frame.generate_centre_3d(
+                        error_threshold=50,
+                        try_experiment_cams=True,
+                        try_all_cams=False,
+                        only_replace_if_better=True,
+                        store_bad_result=True,
+                        ratio_adj_orig=0.1,
+                        ratio_adj_exp=0.1,
+                    )
+                    if res:
+                        frame.save()
+                    else:
+                        ok = False
+                    frame.reload()
+
+                if ok and len(frame.images) != 3:
                     logger.warning(f'Frame does not have prepared images, generating now.')
                     frame.generate_prepared_images()
                     frame.save()
+                    frame.reload()
 
             assert len(frame.images) == 3, 'Frame does not have prepared images.'
             assert midline.frame.centre_3d is not None, 'Frame does not have 3d centre point available.'
