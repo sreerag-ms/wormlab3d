a36fde3f8829699a20e3a3ce8f3548661e0054d1
VincentLou <sc18cwl@leeds.ac.uk>
2021-07-16 23:18:29 +0100

    Implement DataTables for trials; Show date without time

--- app/views/page/trials.py
+++ app/views/page/trials.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template
 
 from wormlab3d.data.model import Trial
 
+# Form blueprint
 bp_trials = Blueprint('trials', __name__)
 
 
@@ -11,9 +12,14 @@ bp_trials = Blueprint('trials', __name__)
 def trials():
     active = 'trials'
     os.environ['script_name'] = active
+    print(Trial.objects.first().date)
     return render_template(
         'trials.html',
         title='Trials',
         active=active,
-        trials=Trial.objects
+        ajax_url="/ajax/trials",
+        source_headers=["_id", "experiment", "date", "trial_num",
+                        "num_frames", "fps", "temperature", "comments"],
+        date_headers=["date"],
+        trials=Trial.objects.none()   # Let views.api.trials.ajax_trials do the query
     )
