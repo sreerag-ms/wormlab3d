565d62aab2973095b62c15ddcc64c5eeb75268ab
tom0 <>
2021-09-29 15:33:45 +0100

    Generate prepared images script minor bug fix

--- scripts/preprocessing/generate_prepared_images.py
+++ scripts/preprocessing/generate_prepared_images.py
@@ -25,9 +25,9 @@ def generate_prepared_images(
     trials, _ = resolve_targets(experiment_id, trial_id, frame_num=frame_num)
     trial_ids = [t.id for t in trials]
     if missing_only:
-        logger.info(f'Generating any missing prepared images for {trials.count()} trials.')
+        logger.info(f'Generating any missing prepared images for {len(trial_ids)} trials.')
     else:
-        logger.info(f'(Re)generating ALL prepared images for {trials.count()} trials.')
+        logger.info(f'(Re)generating ALL prepared images for {len(trial_ids)} trials.')
 
     # Iterate over matching trials
     for trial_id in trial_ids:
