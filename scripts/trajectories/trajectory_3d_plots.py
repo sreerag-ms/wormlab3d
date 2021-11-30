dae186eeaf442a638871f068a1e15f09a4d78171
tom0 <>
2021-11-30 14:39:13 +0000

    3D trajectory animation videos

--- scripts/trajectories/trajectory_3d_plots.py
+++ scripts/trajectories/trajectory_3d_plots.py
@@ -3,10 +3,14 @@ from argparse import Namespace
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import animation
 from matplotlib.axes import Axes
 from matplotlib.gridspec import GridSpec
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
-from wormlab3d import LOGS_PATH, START_TIMESTAMP
+
+from simple_worm.frame import FrameSequenceNumpy
+from simple_worm.plot3d import FrameArtist
+from wormlab3d import LOGS_PATH, START_TIMESTAMP, logger
 from wormlab3d.simple_worm.estimate_k import get_K_estimates_from_args
 from wormlab3d.toolkit.plot_utils import tex_mode, equal_aspect_ratio
 from wormlab3d.trajectories.args import get_args
@@ -15,9 +19,13 @@ from wormlab3d.trajectories.cache import get_trajectory_from_args
 from wormlab3d.trajectories.pca import get_planarity_from_args
 from wormlab3d.trajectories.util import calculate_speeds, calculate_htd
 
-show_plots = True
+animate = True
+show_plots = False
 save_plots = True
 img_extension = 'png'
+fps = 25
+playback_speed = 10
+n_revolutions = 2
 
 tex_mode()
 
@@ -31,37 +39,113 @@ def get_trajectory(args: Namespace):
 
 def make_plot(
         title: str,
-        X: np.ndarray,
+        X_slice: np.ndarray,
+        X_full: np.ndarray = None,
+        filename: str = None,
         colours: np.ndarray = None,
         cmap: str = 'jet',
         show_colourbar: bool = False,
-        ax: Axes = None
+        ax: Axes = None,
+        draw_worm: bool = True
 ):
-    x, y, z = X.T
-    if colours is None:
-        colours = np.linspace(0, 1, len(X))
+    if X_full is None:
+        draw_worm = False
 
+    x, y, z = X_slice.T
+    if colours is None:
+        colours = np.linspace(0, 1, len(X_slice))
+
+    return_ax = False
     if ax is None:
         fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot(projection='3d')
     else:
         fig = ax.get_figure()
+        return_ax = True
 
     # Scatter the vertices
-    s = ax.scatter(x, y, z, c=colours, cmap=cmap, s=10, alpha=0.4)
+    s = ax.scatter(x, y, z, c=colours, cmap=cmap, s=5, alpha=0.4, zorder=-1)
     if show_colourbar:
         fig.colorbar(s)
 
     # Draw lines connecting points
-    points = X[:, None, :]
+    points = X_slice[:, None, :]
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    lc = Line3DCollection(segments, array=colours, cmap=cmap)
+    lc = Line3DCollection(segments, array=colours, cmap=cmap, zorder=-2)
     ax.add_collection(lc)
+
+    # Add worm
+    if draw_worm:
+        FS = FrameSequenceNumpy(x=X_full.transpose(0, 2, 1))
+        fa = FrameArtist(F=FS[0])
+        fa.add_midline(ax)
 
     equal_aspect_ratio(ax)
 
     ax.set_title(title)
+
+    if return_ax:
+        return ax
+
     fig.tight_layout()
+
+    if animate:
+        # Aspects
+        azims = np.linspace(start=0, stop=360 * n_revolutions, num=len(X_slice))
+        ax.view_init(azim=azims[0])  # elev
+
+        def update(frame_num: int):
+            # Rotate the view.
+            ax.view_init(azim=azims[frame_num])
+
+            # Update the worm
+            if draw_worm:
+                fa.update(FS[frame_num])
+            return ()
+
+        idxs_mask = np.array([i % playback_speed == 0 for i in np.arange(np.round(len(X_slice)))])
+        frame_nums = np.arange(len(X_slice))[idxs_mask].tolist()
+        ani = animation.FuncAnimation(
+            fig,
+            update,
+            frames=frame_nums,
+            blit=True,
+            interval=1 / fps
+        )
+
+    if save_plots:
+        assert filename is not None
+        path = LOGS_PATH + '/' + START_TIMESTAMP + f'_{filename}'
+        if animate:
+            metadata = dict(
+                title=title,
+                artist='WormLab Leeds'
+            )
+            save_path = path + '.mp4'
+            logger.info(f'Saving animation to {save_path}.')
+            ani.save(save_path, writer='ffmpeg', fps=fps, metadata=metadata)
+        else:
+            save_path = path + f'.{img_extension}'
+            logger.info(f'Saving plot to {save_path}.')
+            plt.savefig(save_path)
+
+    if show_plots:
+        plt.show()
+
+
+def plot_trajectory():
+    """
+    Draw the trajectory coloured by the time elapsed.
+    """
+    args = get_args()
+    X_full, X_slice = get_trajectory(args)
+
+    make_plot(
+        title=f'Trial {args.trial}.',
+        filename=f'trajectory_trial={args.trial}_{args.midline3d_source}',
+        X_full=X_full,
+        X_slice=X_slice,
+    )
 
 
 def plot_trajectory_head_tail_distance():
@@ -71,26 +155,16 @@ def plot_trajectory_head_tail_distance():
     args = get_args()
     X_full, X_slice = get_trajectory(args)
     htd = calculate_htd(X_full)
-    # scores = (ht_distances - ht_distances.min()) / (ht_distances.max() - ht_distances.min())
 
     make_plot(
         title=f'Head-tail distance. Trial {args.trial}.',
-        X=X_slice,
+        filename=f'trajectory_HTD_trial={args.trial}_{args.midline3d_source}',
+        X_full=X_full,
+        X_slice=X_slice,
         colours=htd,
         cmap='OrRd_r',
         show_colourbar=True
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_trajectory_HTD'
-            f'_trial={args.trial}'
-            f'_{args.midline3d_source}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_trajectory_signed_speed():
@@ -103,22 +177,13 @@ def plot_trajectory_signed_speed():
 
     make_plot(
         title=f'Speed. Trial {args.trial}.',
-        X=X_slice,
+        filename=f'trajectory_signed-speed_trial={args.trial}_{args.midline3d_source}',
+        X_full=X_full,
+        X_slice=X_slice,
         colours=signed_speeds,
         cmap='PRGn',
         show_colourbar=True
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_trajectory_signed-speed'
-            f'_trial={args.trial}'
-            f'_{args.midline3d_source}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_trajectory_K():
@@ -130,23 +195,15 @@ def plot_trajectory_K():
     K_ests = np.log(get_K_estimates_from_args(args))
 
     make_plot(
-        title=f'log(K_est). Trial {args.trial}.',
-        X=X_slice,
+        title=f'log(K\_est). Trial {args.trial}.',
+        filename=f'trajectory_K-est_trial={args.trial}_{args.midline3d_source}',
+        X_full=X_full,
+        X_slice=X_slice,
         colours=K_ests,
         cmap='OrRd_r',
-        show_colourbar=True
+        show_colourbar=True,
+        draw_worm=True
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_trajectory_K-est'
-            f'_trial={args.trial}'
-            f'_{args.midline3d_source}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_trajectory_planarity():
@@ -159,23 +216,13 @@ def plot_trajectory_planarity():
 
     make_plot(
         title=f'Planarity. Trial {args.trial}. PCA window: {args.planarity_window} frames.',
-        X=X_slice,
+        filename=f'trajectory_planarity_w={args.planarity_window}_trial={args.trial}_{args.midline3d_source}',
+        X_full=X_full,
+        X_slice=X_slice,
         colours=planarity,
         cmap='OrRd',
         show_colourbar=True
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_trajectory_planarity'
-            f'_w={args.planarity_window}'
-            f'_trial={args.trial}'
-            f'_{args.midline3d_source}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_brownian_trajectory():
@@ -190,18 +237,9 @@ def plot_brownian_trajectory():
 
     make_plot(
         title=f'Brownian particle. D={D}, n\_steps={n_steps}, total\_time={total_time}.',
-        X=X,
+        filename=f'brownian_particle_D={D}_n={n_steps}_T={total_time}',
+        X_slice=X,
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_brownian_particle'
-            f'_D={D}_n={n_steps}_T={total_time}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_active_particle_trajectory():
@@ -217,18 +255,9 @@ def plot_active_particle_trajectory():
 
     make_plot(
         title=f'Active particle. D={D}, momentum={momentum:.1f}, n\_steps={n_steps}, total\_time={total_time}.',
-        X=X,
+        filename=f'active_particle_D={D}_m={momentum:.2f}_n={n_steps}_T={total_time}',
+        X_slice=X,
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_active_particle'
-            f'_D={D}_m={momentum:.2f}_n={n_steps}_T={total_time}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_active_particle_trajectories():
@@ -250,7 +279,7 @@ def plot_active_particle_trajectories():
             ax = fig.add_subplot(gs[row_idx, col_idx], projection='3d')
             make_plot(
                 title=f'Momentum={momenta[i]:.2f}.',
-                X=X,
+                X_slice=X,
                 ax=ax
             )
             i += 1
@@ -284,33 +313,24 @@ def plot_bounded_particle_trajectory():
 
     make_plot(
         title=f'Bounded particle. D={D}, momentum={momentum:.2f}, n\_steps={n_steps}, total\_time={total_time}.',
-        X=X,
+        filename=f'bounded_particle_D={D}_m={momentum:.2f}_n={n_steps}_T={total_time}',
+        X_slice=X,
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_bounded_particle'
-            f'_D={D}_m={momentum:.2f}_n={n_steps}_T={total_time}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 def plot_confined_particle_trajectory():
     """
     Generate and plot trajectory of a randomly generated brownian particle with momentum and confinement.
     """
-    D = 10
-    momentum = 0.8
-    n_steps = 1000
-    total_time = 10
-    unconfined_duration_mean = 1
-    unconfined_duration_variance = 0.1
-    confined_duration_mean = 3
-    confined_duration_variance = 0.1
-    D_confined = 0.05
+    total_time = 13 * 60
+    n_steps = total_time * 25
+    D = 1
+    momentum = 0.95
+    unconfined_duration_mean = 30
+    unconfined_duration_variance = 1
+    confined_duration_mean = 30
+    confined_duration_variance = 1
+    D_confined = 0.01
 
     p = ConfinedParticle(
         D=D,
@@ -328,20 +348,11 @@ def plot_confined_particle_trajectory():
               f'D\_unconfined={D}, D\_confined={D_confined}, momentum={momentum:.2f}, n\_steps={n_steps}, total\_time={total_time}.\n'
               f'Unconfined duration $\sim \mathcal{{N}}({unconfined_duration_mean:.2f}, {unconfined_duration_variance:.2f})$. \n'
               f'Confined duration $\sim \mathcal{{N}}({confined_duration_mean:.2f}, {unconfined_duration_variance:.2f})$.',
-        X=X,
+        filename=f'confined_particle_Du={D}_Dc={D_confined}_m={momentum:.2f}_n={n_steps}_T={total_time}'
+                 f'_u={unconfined_duration_mean:.2f},{unconfined_duration_variance:.2f}'
+                 f'_c={unconfined_duration_mean:.2f},{unconfined_duration_variance:.2f}',
+        X_slice=X,
     )
-
-    if save_plots:
-        plt.savefig(
-            LOGS_PATH + '/' + START_TIMESTAMP +
-            f'_confined_particle'
-            f'_Du={D}_Dc={D_confined}_m={momentum:.2f}_n={n_steps}_T={total_time}'
-            f'_u={unconfined_duration_mean:.2f},{unconfined_duration_variance:.2f}'
-            f'_c={unconfined_duration_mean:.2f},{unconfined_duration_variance:.2f}'
-            f'.{img_extension}'
-        )
-    if show_plots:
-        plt.show()
 
 
 if __name__ == '__main__':
@@ -349,12 +360,13 @@ if __name__ == '__main__':
         os.makedirs(LOGS_PATH, exist_ok=True)
     # from simple_worm.plot3d import interactive
     # interactive()
-    plot_trajectory_head_tail_distance()
-    plot_trajectory_signed_speed()
-    plot_trajectory_K()
+    # plot_trajectory()
+    # plot_trajectory_head_tail_distance()
+    # plot_trajectory_signed_speed()
+    # plot_trajectory_K()
     plot_trajectory_planarity()
-    plot_brownian_trajectory()
-    plot_active_particle_trajectory()
-    plot_active_particle_trajectories()
-    plot_bounded_particle_trajectory()
-    plot_confined_particle_trajectory()
+    # plot_brownian_trajectory()
+    # plot_active_particle_trajectory()
+    # plot_active_particle_trajectories()
+    # plot_bounded_particle_trajectory()
+    # plot_confined_particle_trajectory()
