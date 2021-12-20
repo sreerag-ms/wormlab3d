dac02f093b7711720207bccb693d74fec9c08dee
tom0 <>
2021-12-20 09:44:05 +0000

    Add frames tab to trial view with image viewer

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,6 +1,7 @@
 from app.model.document_view import DocumentView
 from app.model.dataset import DatasetsView
 from app.model.experiment import ExperimentView
+from app.model.frame import FrameView
 from app.model.midline2d import Midlines2dView
 from app.model.midline3d import Midlines3dView
 from app.model.reconstruction import ReconstructionView
---
+++ app/model/frame.py
@@ -0,0 +1,61 @@
+from collections import OrderedDict
+from typing import Dict
+
+from app.model.document_view import DocumentView
+from wormlab3d.data.model import Frame
+
+
+class FrameView(DocumentView):
+    has_item_view = False
+
+    @classmethod
+    @property
+    def document_class(cls):
+        return Frame
+
+    def _init_fields(self) -> OrderedDict[str, Dict[str, str]]:
+        # frame_num = IntField(required=True)
+        # max_brightnesses = TripletField(IntField())
+        # centres_2d = TripletField(ListField(ListField()))
+        # centres_2d_thresholds = TripletField(IntField())
+        # centre_3d = EmbeddedDocumentField(ObjectPoint)
+        # tags = ListField(ReferenceField(Tag))
+
+        return OrderedDict([
+            (
+                self.prefix + 'trial', {
+                    'title': 'Trial',
+                    'type': 'relation',
+                    'filter_type': 'integer',
+                    'early_match': True
+                },
+            ),
+            (
+                self.prefix + 'frame_num', {
+                    'title': 'Frame num.',
+                    'type': 'integer',
+                },
+            ),
+            (
+                self.prefix + 'max_brightnesses', {
+                    'title': 'Max brightnesses',
+                    'type': 'array',
+                    'type_array': 'integer',
+                },
+            ),
+            (
+                self.prefix + 'centre_3d.point_3d', {
+                    'title': 'Centre 3D',
+                    'type': 'array',
+                    'type_array': 'float',
+                    'precision': 2,
+                },
+            ),
+            (
+                self.prefix + 'centre_3d.error', {
+                    'title': 'Triangulation error',
+                    'type': 'float',
+                    'precision': 3,
+                },
+            )
+        ])
