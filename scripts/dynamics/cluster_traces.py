625c8676f152fe41cec1b46bd44190ca6adf97d5
tom0 <>
2022-03-31 09:54:05 +0100

    DC - cluster traces plots - add cluster matrices and more options

--- scripts/dynamics/cluster_traces.py
+++ scripts/dynamics/cluster_traces.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from fastcluster import linkage
-from scipy.cluster.hierarchy import fcluster
+from matplotlib import gridspec
+from scipy.cluster.hierarchy import fcluster, cophenet
+from scipy.spatial.distance import squareform, pdist
 
 from wormlab3d import LOGS_PATH, START_TIMESTAMP
 from wormlab3d import logger
@@ -14,15 +16,16 @@ from wormlab3d.data.model import Reconstruction, Checkpoint
 from wormlab3d.dynamics.args import DynamicsNetworkArgs, DynamicsRuntimeArgs, DynamicsDatasetArgs, DynamicsOptimiserArgs
 from wormlab3d.dynamics.manager import Manager
 from wormlab3d.nn.args import NetworkArgs
-from wormlab3d.toolkit.util import print_args, to_numpy
+from wormlab3d.toolkit.plot_utils import fancy_dendrogram, plot_reordered_distances
+from wormlab3d.toolkit.util import print_args, to_numpy, str2bool
 from wormlab3d.trajectories.cache import get_trajectory
 from wormlab3d.trajectories.pca import generate_or_load_pca_cache
 from wormlab3d.trajectories.util import calculate_speeds
 
 # tex_mode()
 
-show_plots = True
-save_plots = False
+show_plots = False
+save_plots = True
 img_extension = 'png'
 
 
@@ -34,23 +37,52 @@ def parse_args() -> Namespace:
                         help='Reconstruction by id.')
     parser.add_argument('--start-frame', type=int, help='Frame number to start from.')
     parser.add_argument('--end-frame', type=int, help='Frame number to end at.')
+    parser.add_argument('--distance-metric', type=str, help='Distance metric.')
     parser.add_argument('--linkage-method', type=str, help='Linkage method to use.')
-    parser.add_argument('--min-cluster', type=str, help='Minimum number of clusters to plot.')
-    parser.add_argument('--max-cluster', type=str, help='Maximum number of clusters to plot.')
+    parser.add_argument('--min-clusters', type=int, help='Minimum number of clusters to plot.')
+    parser.add_argument('--max-clusters', type=int, help='Maximum number of clusters to plot.')
+    parser.add_argument('--step', type=int, help='Window step size.')
+    parser.add_argument('--plot-traces', type=str2bool, help='Plot the traces.')
+    parser.add_argument('--plot-matrices', type=str2bool, help='Plot the matrices.')
 
     args = parser.parse_args()
     assert args.reconstruction is not None, 'This script requires setting --reconstruction=id.'
+    assert args.step > 0, 'Step size must be > 0.'
+    assert args.plot_traces or args.plot_matrices, 'One of the plot options must be set.'
 
     print_args(args)
 
     return args
 
 
-def traces(
-        linkage_method: str = 'ward',
-        min_clusters: int = 3,
-        max_clusters: int = 7
-):
+def find_best_linkage_settings(X):
+    # Find best linkage method using cophenetic correlation coefficient
+    linkage_methods = ['single', 'complete', 'average', 'centroid', 'median', 'ward', 'weighted']
+    metrics = ['canberra', 'cityblock', 'euclidean', 'hamming', 'matching', 'sqeuclidean', 'seuclidean', 'cosine',
+               'correlation']
+    best_score = 0
+    best_method = 'average'
+    best_metric = 'euclidean'
+    for method in linkage_methods:
+        for metric in metrics:
+            try:
+                Z = linkage(X, method, metric)
+                c, coph_dists = cophenet(Z, pdist(X, metric))
+            except Exception:
+                continue
+            print('Method = {}. Metric = {}. Score = {}'.format(method, metric, c))
+            if c > best_score:
+                best_score = c
+                best_method = method
+                best_metric = metric
+    print('Best combination: Method = {}. Metric = {}. Score = {}'.format(best_method, best_metric, best_score))
+    return best_method, best_metric
+
+
+def make_cluster_plots():
+    """
+    Generate some cluster plots.
+    """
     args = parse_args()
 
     # Load the checkpoint
@@ -85,21 +117,6 @@ def traces(
         'smoothing_window': dataset_args.smoothing_window,
     }
 
-    reconstruction = Reconstruction.objects.get(id=args.reconstruction)
-    X_traj, meta = get_trajectory(**common_args)
-    N = len(X_traj)
-    ts = np.linspace(0, N / reconstruction.trial.fps, N)
-
-    # Speed
-    logger.info('Calculating speeds.')
-    speeds = calculate_speeds(X_traj, signed=True)
-
-    # Planarity
-    logger.info('Fetching planarities.')
-    pcas, meta = generate_or_load_pca_cache(**common_args, window_size=1)
-    r = pcas.explained_variance_ratio.T
-    nonp = r[2] / np.sqrt(r[1] * r[0])
-
     # Get natural frame trajectory
     X_nf, meta = get_trajectory(**common_args, natural_frame=True)
 
@@ -125,11 +142,10 @@ def traces(
     # Slide along sequence with 3/4 overlap collecting all the data into batches.
     start = 0
     ws = manager.dataset_args.sample_duration
-    step = int(ws / 4)
     Xs = []
     while start + ws < len(X):
         Xs.append(torch.from_numpy(X[start:start + ws].T))
-        start += step
+        start += args.step
     Xs = torch.stack(Xs).to(torch.float32).to(manager.device)
     batches = Xs.split(manager.runtime_args.batch_size)
     logger.info(f'{len(batches)} batches generated from reconstruction.')
@@ -141,20 +157,48 @@ def traces(
     for batch in batches:
         Z = manager.net.classifier_net.forward(batch)
         Zs.append(Z)
-    Zs = torch.cat(Zs, dim=0)
+    Zs = to_numpy(torch.cat(Zs, dim=0))
 
     # Calculate pairwise distances
-    logger.info('Calculating pairwise distances.')
-    distances = torch.pdist(Zs, p=2)
-    distances = to_numpy(distances)
-    # distances_sf = squareform(distances)
+    # find_best_linkage_settings(Zs)
+    logger.info(f'Calculating pairwise distances using metric "{args.distance_metric}".')
+    distances = pdist(Zs, args.distance_metric)
+    distances = distances
 
     # Calculate linkage
-    logger.info(f'Calculating linkage using method "{linkage_method}".')
-    L = linkage(distances, linkage_method)
+    logger.info(f'Calculating linkage using method "{args.linkage_method}".')
+    L = linkage(distances, args.linkage_method)
+
+    if args.plot_traces:
+        _plot_cluster_trace(args, common_args, checkpoint, L)
+
+    if args.plot_matrices:
+        _plot_matrices(args, checkpoint, L, squareform(distances))
+
+
+def _plot_cluster_trace(
+        args: Namespace,
+        common_args: dict,
+        checkpoint: Checkpoint,
+        L,
+):
+    reconstruction = Reconstruction.objects.get(id=args.reconstruction)
+    X_traj, meta = get_trajectory(**common_args)
+    N = len(X_traj)
+    ts = np.linspace(0, N / reconstruction.trial.fps, N)
+
+    # Speed
+    logger.info('Calculating speeds.')
+    speeds = calculate_speeds(X_traj, signed=True)
+
+    # Planarity
+    logger.info('Fetching planarities.')
+    pcas, meta = generate_or_load_pca_cache(**common_args, window_size=1)
+    r = pcas.explained_variance_ratio.T
+    nonp = r[2] / np.sqrt(r[1] * r[0])
 
     # Plot
-    n_cluster_plots = max_clusters - min_clusters + 1
+    n_cluster_plots = args.max_clusters - args.min_clusters + 1
     fig, axes = plt.subplots(2 + n_cluster_plots, figsize=(16, (2 + n_cluster_plots) * 3), sharex=True)
 
     # Speeds
@@ -171,11 +215,13 @@ def traces(
     ax.grid()
 
     # Cluster plots
-    cluster_nums = list(range(min_clusters, max_clusters + 1))
+    cmap = plt.get_cmap('jet')
+    cluster_nums = list(range(args.min_clusters, args.max_clusters + 1))
     for i, n_clusters in enumerate(cluster_nums):
         logger.info(f'Clustering into {n_clusters} clusters.')
         clusters = fcluster(L, n_clusters, criterion='maxclust')
         y_locs = np.arange(1, n_clusters + 1)
+        colours = cmap(np.linspace(0, 1, n_clusters))
 
         ax = axes[2 + i]
         ax.set_title(f'Clusters = {n_clusters}.')
@@ -186,9 +232,9 @@ def traces(
             zs = np.argwhere(clusters == cluster_idx)[:, 0]
             xs = []
             for z in zs:
-                xs.extend(np.arange(z * step, (z + 1) * step))
+                xs.extend(np.arange(z * args.step, (z + 1) * args.step))
             ys = np.ones_like(xs) * y_locs[j]
-            ax.scatter(ts[xs], ys, label=cluster_idx)
+            ax.scatter(ts[xs], ys, label=cluster_idx, color=colours[j])
 
         ax.set_yticks(y_locs)
         ax.grid()
@@ -198,16 +244,81 @@ def traces(
     title = f'Trial={reconstruction.trial.id}. Reconstruction={reconstruction.id}. Checkpoint={checkpoint.id}.'
     if args.start_frame is not None:
         title += f'\nFrames={args.start_frame}-{args.end_frame}'
-    title += f'\nLinkage method: {linkage_method}.'
+    title += f'\nLinkage method: {args.linkage_method}. Distance metric: {args.distance_metric}.'
     fig.suptitle(title)
     fig.tight_layout()
 
     if save_plots:
         path = LOGS_PATH / f'{START_TIMESTAMP}_cluster_traces' \
                            f'_cp={checkpoint.id}' \
-                           f'_l={linkage_method}' \
-                           f'_c={min_clusters}-{max_clusters}' \
+                           f'_l={args.linkage_method}' \
+                           f'_c={args.min_clusters}-{args.max_clusters}' \
                            f'_r={reconstruction.id}' \
+                           f'_f={args.start_frame}-{args.end_frame}' \
+                           f'.{img_extension}'
+        logger.info(f'Saving plot to {path}.')
+        plt.savefig(path)
+
+    if show_plots:
+        plt.show()
+
+
+def _plot_matrices(
+        args: Namespace,
+        checkpoint: Checkpoint,
+        L: np.ndarray,
+        distances_sf: np.ndarray
+):
+    # Set up plots
+    n_cluster_plots = args.max_clusters - args.min_clusters + 1
+    n_cluster_plot_rows = int(np.ceil(n_cluster_plots / 3))
+    n_rows = 1 + n_cluster_plot_rows
+    n_cols = 3
+    fig = plt.figure(figsize=(n_cols * 4, n_rows * 4))
+    gs = gridspec.GridSpec(n_rows, n_cols, fig)
+
+    # Show original data
+    ax = plt.subplot(gs[0, 0])
+    ax.matshow(distances_sf, cmap=plt.cm.Blues)
+    ax.set_title('Distances between encodings')
+
+    # Calculate and plot full dendrogram
+    ax = plt.subplot(gs[0, 1:])
+    fancy_dendrogram(
+        ax,
+        L,
+        truncate_mode='lastp',  # show only the last p merged clusters
+        p=12,  # show only the last p merged clusters
+        show_leaf_counts=True,  # otherwise numbers in brackets are counts
+        leaf_rotation=0.,
+        leaf_font_size=12.,
+        show_contracted=True,  # to get a distribution impression in truncated branches
+        annotate_above=1,  # useful in small plots so annotations don't overlap,
+    )
+    cluster_nums = list(range(args.min_clusters, args.max_clusters + 1))
+    cluster_idx = 0
+    for row_idx in range(n_cluster_plot_rows):
+        for col_idx in range(3):
+            if cluster_idx >= len(cluster_nums):
+                break
+            n_clusters = cluster_nums[cluster_idx]
+            clusters = fcluster(L, n_clusters, criterion='maxclust')
+            ax = plt.subplot(gs[row_idx + 1, col_idx])
+            plot_reordered_distances(ax, distances_sf, clusters)
+            ax.axis('off')
+            cluster_idx += 1
+
+    fig.suptitle(f'Reconstruction: {args.reconstruction}. '
+                 f'Linkage method: {args.linkage_method}. '
+                 f'Distance metric: {args.distance_metric}.')
+    fig.tight_layout()
+
+    if save_plots:
+        path = LOGS_PATH / f'{START_TIMESTAMP}_distances' \
+                           f'_cp={checkpoint.id}' \
+                           f'_l={args.linkage_method}' \
+                           f'_c={args.min_clusters}-{args.max_clusters}' \
+                           f'_r={args.reconstruction}' \
                            f'_f={args.start_frame}-{args.end_frame}' \
                            f'.{img_extension}'
         logger.info(f'Saving plot to {path}.')
@@ -222,4 +333,4 @@ if __name__ == '__main__':
         os.makedirs(LOGS_PATH, exist_ok=True)
     # from simple_worm.plot3d import interactive
     # interactive()
-    traces()
+    make_cluster_plots()
