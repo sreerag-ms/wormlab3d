MF - Add curvature relaxation factor. Parameterise min sigmas and intensities. Fix min sigma init bug. Clamp camera scaling factors to not go too extreme.

wormlab3d/midlines3d/frame_state.py
@@ -224,8 +224,9 @@ class FrameState(nn.Module):
 
         # Initialise the sigmas
         sigmas = []
+        sigmas_init = max(mp.sigmas_min + 0.01, mp.sigmas_init)
         for d in range(mp.depth_min, mp.depth):
-            sigmas_d = nn.Parameter(torch.tensor(mp.sigmas_init), requires_grad=mp.optimise_sigmas)
+            sigmas_d = nn.Parameter(torch.tensor(sigmas_init), requires_grad=mp.optimise_sigmas)
             sigmas.append(sigmas_d)
         self.register_parameter('sigmas', sigmas)
 

wormlab3d/midlines3d/midline3d_finder.py
@@ -173,6 +173,8 @@ class Midline3DFinder:
         model = ProjectRenderScoreModel(
             image_size=PREPARED_IMAGE_SIZE[0],
             render_mode=self.parameters.render_mode,
+            sigmas_min=self.parameters.sigmas_min,
+            intensities_min=self.parameters.intensities_min,
             curvature_mode=self.parameters.curvature_mode,
             curvature_deltas=self.parameters.curvature_deltas,
             length_min=self.parameters.length_min,
@@ -218,7 +220,7 @@ class Midline3DFinder:
         reconstruction = None
         try:
             reconstruction = Reconstruction.objects.get(**params)
-            if reconstruction.start_frame < start_frame:
+            if reconstruction.start_frame > start_frame:
                 reconstruction.start_frame = start_frame
                 reconstruction.save()
             logger.info(f'Loaded reconstruction (id={reconstruction.id}, created={reconstruction.created}).')
@@ -242,28 +244,6 @@ class Midline3DFinder:
         """
         logger.info('Initialising trial state.')
         self.trial: Trial = Trial.objects.get(id=self.source_args.trial_id)
-
-        # Look for existing reconstruction
-        reconstruction = None
-        try:
-            reconstruction = Reconstruction.objects.get(
-                trial=self.trial,
-                source=M3D_SOURCE_MF,
-                mf_parameters=self.parameters,
-            )
-        except DoesNotExist:
-            pass
-
-        if reconstruction is None:
-            reconstruction = Reconstruction(
-                trial=self.trial,
-                source=M3D_SOURCE_MF,
-                mf_parameters=self.parameters,
-                start_frame=self.source_args.start_frame,
-                end_frame=self.source_args.start_frame
-            )
-            reconstruction.save()
-            logger.info('Created reconstruction')
 
         # Prepare trial state
         self.trial_state = TrialState(
@@ -602,6 +582,14 @@ class Midline3DFinder:
                 prev_frame_state=mfs,
                 device=self.device
             )
+
+            # Reduce curvature
+            if p.curvature_mode and p.curvature_relaxation_factor is not None:
+                K = next_frame.get_state('curvatures')
+                with torch.no_grad():
+                    for K_d in K:
+                        K_d.data = K_d * p.curvature_relaxation_factor
+
             if p.use_master:
                 mfs.frame_num = next_frame.frame_num
                 mfs.copy_state(next_frame)
@@ -1052,9 +1040,12 @@ class Midline3DFinder:
                 for v in params:
                     v.data = v.clamp(**render_parameters_limits[sei])
 
-                # Camera scaling factors should average 1
+                # Camera scaling factors should average 1 and not be more than 20% from the mean
                 v = self.master_frame_state.get_state(f'camera_{sei}')
                 v.data = v / v.mean()
+                sf = 0.2 / ((v - 1).abs()).amax()
+                if sf < 1:
+                    v.data = (v - 1) * sf + 1
 
     def _log_progress(self, step: int, final_step: int, loss: float, stats: dict):
         """

wormlab3d/midlines3d/project_render_score.py
@@ -170,6 +170,8 @@ class ProjectRenderScoreModel(nn.Module):
             self,
             image_size: int = PREPARED_IMAGE_SIZE[0],
             render_mode: str = RENDER_MODE_GAUSSIANS,
+            sigmas_min: float = 0.04,
+            intensities_min: float = 0.4,
             curvature_mode: bool = False,
             curvature_deltas: bool = False,
             length_min: float = 0.5,
@@ -184,6 +186,8 @@ class ProjectRenderScoreModel(nn.Module):
         self.image_size = image_size
         assert render_mode in RENDER_MODES
         self.render_mode = render_mode
+        self.sigmas_min = sigmas_min
+        self.intensities_min = intensities_min
         self.curvature_mode = curvature_mode
         self.curvature_deltas = curvature_deltas
         self.length_min = length_min
@@ -371,9 +375,8 @@ class ProjectRenderScoreModel(nn.Module):
                 N4 = int(N / 4)
 
                 # Sigmas should be equal in the middle section but taper towards the ends
-                min_sigma = 0.04
-                sigma_d = sigmas_d[:, None].clamp(min=min_sigma)
-                slopes = (sigma_d - min_sigma) / N4 * torch.arange(N4, device=device)[None, :] + min_sigma
+                sigma_d = sigmas_d[:, None].clamp(min=self.sigmas_min)
+                slopes = (sigma_d - self.sigmas_min) / N4 * torch.arange(N4, device=device)[None, :] + self.sigmas_min
                 sigmas_d = torch.cat([
                     slopes,
                     torch.ones(bs, 2 * N4, device=device) * sigma_d,
@@ -384,9 +387,8 @@ class ProjectRenderScoreModel(nn.Module):
                 exponents_d = torch.ones(bs, N, device=device) * exponents_d[:, None]
 
                 # Intensities should be equal in the middle section but taper towards the ends
-                min_int = 0.4
                 int_d = intensities_d[:, None]
-                slopes = (int_d - min_int) / N4 * torch.arange(N4, device=device)[None, :] + min_int
+                slopes = (int_d - self.intensities_min) / N4 * torch.arange(N4, device=device)[None, :] + self.intensities_min
                 intensities_d = torch.cat([
                     slopes,
                     torch.ones(bs, 2 * N4, device=device) * int_d,