4111bc81623ff69a178c76af7250c37caaa75423
tom0 <>
2022-10-17 10:48:59 +0100

    MF plots - highlighted blob idxs in stacks, colourful borders, intersections

--- scripts/midlines3d/plot_mf_renders.py
+++ scripts/midlines3d/plot_mf_renders.py
@@ -1,6 +1,7 @@
 import os
 from typing import Tuple, Optional, List, Union
 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -49,7 +50,7 @@ def _get_targets() -> Tuple[Reconstruction, Frame]:
 def _get_reds(
         alpha_min: float = 0.3,
         white_at: float = 0.1
-):
+) -> np.ndarray:
     """
     Get the alpha-blended red colours.
     """
@@ -59,6 +60,21 @@ def _get_reds(
     reds[:int(255 * white_at), :3] = (1, 1, 1)
     reds = (reds * 255).astype(np.uint8)
     return reds
+
+
+def _make_blob_img(blob: np.ndarray, lut: np.ndarray, border_size: Optional[int],
+                   border_colour: np.ndarray) -> Image.Image:
+    """
+    Make a blob image using colours from the look-up-table with a border as defined.
+    """
+    b = np.take(lut, blob, axis=0)
+    if border_size > 0:
+        b[:border_size, :] = border_colour
+        b[-border_size:, :] = border_colour
+        b[:, :border_size] = border_colour
+        b[:, -border_size:] = border_colour
+    b_img = Image.fromarray(b)
+    return b_img
 
 
 class RenderWrapper:
@@ -259,7 +275,8 @@ def plot_blob_stacks(
         amplification_factor: float = 1.1,
         alpha_min: float = 0.3,
         white_at: float = 0.1,
-        border_colour: Tuple[int] = (0, 0, 0, 200),
+        border_size: int = 1,
+        border_colour: Optional[Tuple[int]] = (0, 0, 0, 200),
         n_blobs: int = 33,
         blob_spacing: int = 6,
         blob_chunk_spacing: int = 40,
@@ -267,6 +284,10 @@ def plot_blob_stacks(
         show_last_n_blobs: int = 1,
         n_dots: int = 3,
         dot_size: int = 2,
+        dot_colour: Tuple[int] = (0, 0, 0, 200),
+        highlight_idx: Optional[int] = None,
+        highlight_offset: int = 100,
+        highlight_border_size: int = 2,
 ):
     """
     Plot stacks of rendered blobs for a midline.
@@ -275,6 +296,11 @@ def plot_blob_stacks(
     N = reconstruction.mf_parameters.n_points_total
     reds = _get_reds(alpha_min=alpha_min, white_at=white_at)
     masks, blobs = _get_masks_and_blobs(reconstruction, frame)
+    if border_colour is None:
+        cmap = plt.get_cmap(MIDLINE_CMAP_DEFAULT)
+        border_colours = cmap(np.linspace(0, 1, N)) * 255
+    else:
+        border_colours = np.ones((N, 4)) * border_colour
 
     # Amplify the blobs a little and convert to 8-bit
     blobs = ((blobs * amplification_factor).clip(max=1) * 255).astype(np.uint8)
@@ -288,6 +314,8 @@ def plot_blob_stacks(
     # Calculate the offsets
     offsets_first = np.arange(show_first_n_blobs) * blob_spacing
     offsets_last = np.arange(show_last_n_blobs) * blob_spacing
+    highlight_rhs = trial.crop_size + highlight_offset \
+                    + int((show_first_n_blobs - 0.5) * blob_spacing + blob_chunk_spacing / 2)
 
     # Prepare path
     save_dir = LOGS_PATH / (f'{START_TIMESTAMP}_blob_stacks'
@@ -303,6 +331,7 @@ def plot_blob_stacks(
             amplification_factor=amplification_factor,
             alpha_min=alpha_min,
             white_at=white_at,
+            border_size=border_size,
             border_colour=border_colour,
             n_blobs=n_blobs,
             blob_spacing=blob_spacing,
@@ -311,27 +340,26 @@ def plot_blob_stacks(
             show_last_n_blobs=show_last_n_blobs,
             n_dots=n_dots,
             dot_size=dot_size,
+            dot_colour=dot_colour,
+            highlight_idx=highlight_idx,
+            highlight_offset=highlight_offset,
+            highlight_border_size=highlight_border_size
         )
         with open(save_dir / 'spec.yml', 'w') as f:
             yaml.dump(spec, f)
 
-    def _make_blob_img(c_, n_):
-        b_ = np.take(reds, blobs[c_, n_], axis=0)
-        b_[0, :] = border_colour
-        b_[-1, :] = border_colour
-        b_[:, 0] = border_colour
-        b_[:, -1] = border_colour
-        b_img = Image.fromarray(b_)
-        return b_img
-
     for c in range(3):
-        dim = trial.crop_size + (show_first_n_blobs + show_last_n_blobs - 1) * blob_spacing + blob_chunk_spacing
-        bg = np.ones((dim, dim, 4), dtype=np.uint8) * 255
+        dim_y = trial.crop_size + (show_first_n_blobs + show_last_n_blobs - 1) * blob_spacing + blob_chunk_spacing
+        if highlight_idx is not None and dim_y < highlight_rhs:
+            dim_x = highlight_rhs
+        else:
+            dim_x = dim_y
+        bg = np.ones((dim_y, dim_x, 4), dtype=np.uint8) * 255
         blob_stack = Image.fromarray(bg)
 
         # Draw the back of the stack first
         for i, n in enumerate(blob_idxs[::-1][:show_last_n_blobs]):
-            blob_img = _make_blob_img(c, n)
+            blob_img = _make_blob_img(blobs[c, n], reds, border_size, border_colours[n])
             blob_stack.paste(
                 blob_img,
                 box=(
@@ -341,9 +369,22 @@ def plot_blob_stacks(
                 mask=blob_img
             )
 
+        # Add a highlighted frame if requested
+        if highlight_idx is not None:
+            n = blob_idxs[highlight_idx]
+            blob_img = _make_blob_img(blobs[c, n], reds, highlight_border_size, border_colours[n])
+            blob_stack.paste(
+                blob_img,
+                box=(
+                    highlight_rhs - trial.crop_size,
+                    highlight_rhs - trial.crop_size - highlight_offset
+                ),
+                mask=blob_img
+            )
+
         # Draw the front of the stack
         for i, n in enumerate(blob_idxs[:show_first_n_blobs][::-1]):
-            blob_img = _make_blob_img(c, n)
+            blob_img = _make_blob_img(blobs[c, n], reds, border_size, border_colours[n])
             blob_stack.paste(
                 blob_img,
                 box=(
@@ -361,19 +402,20 @@ def plot_blob_stacks(
         for i in range(n_dots):
             x = blob_spacing * show_first_n_blobs + dot_offsets[i]
             y = blob_spacing * show_last_n_blobs + blob_chunk_spacing - dot_offsets[i]
-            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=border_colour)
+            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=dot_colour)
 
-        # Top-right dots
-        for i in range(n_dots):
-            x = trial.crop_size + blob_spacing * (show_first_n_blobs - 1) + dot_offsets[i]
-            y = blob_spacing * show_last_n_blobs + blob_chunk_spacing - dot_offsets[i]
-            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=border_colour)
+        if highlight_idx is None:
+            # Top-right dots
+            for i in range(n_dots):
+                x = trial.crop_size + blob_spacing * (show_first_n_blobs - 1) + dot_offsets[i]
+                y = blob_spacing * show_last_n_blobs + blob_chunk_spacing - dot_offsets[i]
+                draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=dot_colour)
 
-        # Bottom-left dots
-        for i in range(n_dots):
-            x = trial.crop_size + blob_spacing * (show_first_n_blobs - 1) + dot_offsets[i]
-            y = trial.crop_size + blob_spacing * (show_last_n_blobs - 1) + blob_chunk_spacing - dot_offsets[i]
-            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=border_colour)
+            # Bottom-right dots
+            for i in range(n_dots):
+                x = trial.crop_size + blob_spacing * (show_first_n_blobs - 1) + dot_offsets[i]
+                y = trial.crop_size + blob_spacing * (show_last_n_blobs - 1) + blob_chunk_spacing - dot_offsets[i]
+                draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=dot_colour)
 
         if save_plots:
             blob_stack.save(save_dir / f'blob_stack_c{c}.png')
@@ -387,7 +429,7 @@ def plot_blob_stacks_horizontal(
         amplification_factor: float = 1.1,
         alpha_min: float = 0.3,
         white_at: float = 0.1,
-        border_size: int =1,
+        border_size: int = 1,
         border_colour: Optional[Tuple[int]] = (0, 0, 0, 200),
         n_blobs: int = 33,
         blob_spacing: int = 6,
@@ -396,6 +438,7 @@ def plot_blob_stacks_horizontal(
         show_last_n_blobs: int = 1,
         n_dots: int = 3,
         dot_size: int = 2,
+        dot_colour: Tuple[int] = (0, 0, 0, 200),
 ):
     """
     Plot horizontal stacks of rendered blobs for a midline.
@@ -404,8 +447,11 @@ def plot_blob_stacks_horizontal(
     N = reconstruction.mf_parameters.n_points_total
     reds = _get_reds(alpha_min=alpha_min, white_at=white_at)
     masks, blobs = _get_masks_and_blobs(reconstruction, frame)
-    cmap = plt.get_cmap(MIDLINE_CMAP_DEFAULT)
-    border_colours = cmap(np.linspace(0, 1, N)) * 255
+    if border_colour is None:
+        cmap = plt.get_cmap(MIDLINE_CMAP_DEFAULT)
+        border_colours = cmap(np.linspace(0, 1, N)) * 255
+    else:
+        border_colours = np.ones((N, 4)) * border_colour
 
     # Amplify the blobs a little and convert to 8-bit
     blobs = ((blobs * amplification_factor).clip(max=1) * 255).astype(np.uint8)
@@ -418,7 +464,8 @@ def plot_blob_stacks_horizontal(
 
     # Calculate the offsets
     offsets_first = np.arange(show_first_n_blobs) * blob_spacing
-    offsets_last = np.arange(show_last_n_blobs) * blob_spacing
+    offsets_last = trial.crop_size + blob_chunk_spacing \
+                   + (show_first_n_blobs + np.arange(show_last_n_blobs)) * blob_spacing
 
     # Prepare path
     save_dir = LOGS_PATH / (f'{START_TIMESTAMP}_blob_stacks_horizontal'
@@ -443,23 +490,10 @@ def plot_blob_stacks_horizontal(
             show_last_n_blobs=show_last_n_blobs,
             n_dots=n_dots,
             dot_size=dot_size,
+            dot_colour=dot_colour,
         )
         with open(save_dir / 'spec.yml', 'w') as f:
             yaml.dump(spec, f)
-
-    def _make_blob_img(c_, n_):
-        b_ = np.take(reds, blobs[c_, n_], axis=0)
-        if border_size > 0:
-            if border_colour is None:
-                bc = border_colours[n]
-            else:
-                bc = border_colour
-            b_[:border_size, :] = bc
-            b_[-border_size:, :] = bc
-            b_[:, :border_size] = bc
-            b_[:, -border_size:] = bc
-        b_img = Image.fromarray(b_)
-        return b_img
 
     for c in range(3):
         dim_x = trial.crop_size * 2 + (show_first_n_blobs + show_last_n_blobs - 1) * blob_spacing + blob_chunk_spacing
@@ -469,26 +503,19 @@ def plot_blob_stacks_horizontal(
 
         # Draw the back of the stack first
         for i, n in enumerate(blob_idxs[::-1][:show_last_n_blobs]):
-            blob_img = _make_blob_img(c, n)
+            blob_img = _make_blob_img(blobs[c, n], reds, border_size, border_colours[n])
             blob_stack.paste(
                 blob_img,
-                box=(
-                    trial.crop_size + blob_chunk_spacing + show_first_n_blobs * blob_spacing + offsets_last[
-                        show_last_n_blobs - i - 1],
-                    0,
-                ),
+                box=(offsets_last[show_last_n_blobs - i - 1], 0),
                 mask=blob_img
             )
 
         # Draw the front of the stack
         for i, n in enumerate(blob_idxs[:show_first_n_blobs][::-1]):
-            blob_img = _make_blob_img(c, n)
+            blob_img = _make_blob_img(blobs[c, n], reds, border_size, border_colours[n])
             blob_stack.paste(
                 blob_img,
-                box=(
-                    offsets_first[show_first_n_blobs - i - 1],
-                    0,
-                ),
+                box=(offsets_first[show_first_n_blobs - i - 1], 0),
                 mask=blob_img
             )
 
@@ -498,12 +525,72 @@ def plot_blob_stacks_horizontal(
         for i in range(n_dots):
             x = trial.crop_size + blob_spacing * (show_first_n_blobs - 1) + dot_offsets[i]
             y = trial.crop_size / 2
-            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=border_colour)
+            draw.ellipse((x - dot_size, y - dot_size, x + dot_size, y + dot_size), fill=dot_colour)
 
         if save_plots:
             blob_stack.save(save_dir / f'blob_stack_c{c}.png')
         if show_plots:
             blob_stack.show()
+
+
+def plot_blob_intersections(
+        reconstruction: Reconstruction,
+        frame: Frame,
+        blob_idx: int = 0,
+        amplification_factor: float = 1.1,
+        alpha_min: float = 0.3,
+        white_at: float = 0.1
+):
+    """
+    Plot the intersections between blobs for a vertex and an image.
+    """
+    trial = reconstruction.trial
+    reds = _get_reds(alpha_min=alpha_min, white_at=white_at)
+    masks, blobs = _get_masks_and_blobs(reconstruction, frame)
+    blobs = blobs[:, blob_idx]
+
+    # Prepare path
+    save_dir = LOGS_PATH / (f'{START_TIMESTAMP}_blob_{blob_idx}_intersections'
+                            f'_trial={trial.id}'
+                            f'_frame={frame.frame_num}'
+                            f'_reconstruction={reconstruction.id}')
+    if save_plots:
+        os.makedirs(save_dir, exist_ok=True)
+
+        # Save layout spec
+        spec = dict(
+            created=START_TIMESTAMP,
+            amplification_factor=amplification_factor,
+            alpha_min=alpha_min,
+            white_at=white_at,
+            blob_idx=blob_idx
+        )
+        with open(save_dir / 'spec.yml', 'w') as f:
+            yaml.dump(spec, f)
+
+    for c in range(3):
+        blob = (blobs[c] / blobs[c].max() * amplification_factor).clip(max=1)
+        img = frame.images[c]
+        # img[img < reconstruction.mf_parameters.masks_threshold] = 0
+        img /= img.max()
+
+        # Calculate intersection and overlap "score" (not the real score, but close)
+        intersect = blob * img
+        score = intersect.sum()
+
+        # Generate output
+        img = ((1 - img) * 255).astype(np.uint8)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGBA)
+        out = Image.fromarray(img)
+        intersect = (intersect * 255).astype(np.uint8)
+        intersect = np.take(reds, intersect, axis=0)
+        mask = Image.fromarray(intersect)
+        out.paste(mask, mask=mask)
+
+        if save_plots:
+            out.save(save_dir / f'c={c}_s={score:.2f}.png')
+        if show_plots:
+            out.show()
 
 
 def plot_renders(
@@ -733,34 +820,48 @@ if __name__ == '__main__':
     # plot_blob_stacks(
     #     reconstruction=rec_,
     #     frame=frame_,
-    #     amplification_factor=1.1,
-    #     alpha_min=0.3,
-    #     white_at=0.1,
-    #     border_colour=(0, 0, 0, 200),
-    #     n_blobs=33,
-    #     blob_spacing=6,
-    #     blob_chunk_spacing=40,
-    #     show_first_n_blobs=5,
-    #     show_last_n_blobs=1,
+    #     amplification_factor=1,
+    #     alpha_min=0.7,
+    #     white_at=0.05,
+    #     border_size=3,
+    #     # border_colour=(0, 0, 0, 200),
+    #     border_colour=None,
+    #     n_blobs=18,
+    #     blob_spacing=20,
+    #     blob_chunk_spacing=100,
+    #     show_first_n_blobs=3,
+    #     show_last_n_blobs=3,
     #     n_dots=3,
     #     dot_size=2,
+    #     highlight_idx=9,
+    #     highlight_offset=150,
+    #     highlight_border_size=6,
     # )
 
-    plot_blob_stacks_horizontal(
+    # plot_blob_stacks_horizontal(
+    #     reconstruction=rec_,
+    #     frame=frame_,
+    #     amplification_factor=1.,
+    #     alpha_min=0.4,
+    #     white_at=0.1,
+    #     border_size=2,
+    #     border_colour=None,
+    #     n_blobs=16,
+    #     blob_spacing=25,
+    #     blob_chunk_spacing=150,
+    #     show_first_n_blobs=7,
+    #     show_last_n_blobs=3,
+    #     n_dots=5,
+    #     dot_size=3,
+    # )
+
+    plot_blob_intersections(
         reconstruction=rec_,
         frame=frame_,
-        amplification_factor=1.,
-        alpha_min=0.4,
-        white_at=0.1,
-        border_size=2,
-        border_colour=None,
-        n_blobs=16,
-        blob_spacing=25,
-        blob_chunk_spacing=150,
-        show_first_n_blobs=7,
-        show_last_n_blobs=1,
-        n_dots=5,
-        dot_size=3,
+        blob_idx=64,
+        amplification_factor=1.1,
+        alpha_min=0.5,
+        white_at=0.02,
     )
 
     # plot_renders(
