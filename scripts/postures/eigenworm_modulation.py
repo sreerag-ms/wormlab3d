86e5306a03413376605b685d2c8be94f812241d6
tom0 <>
2022-11-29 13:16:07 +0000

    Eigenworm modulation plots and caching

--- scripts/postures/eigenworm_modulation.py
+++ scripts/postures/eigenworm_modulation.py
@@ -1,17 +1,22 @@
 import os
 from argparse import ArgumentParser, Namespace
+from typing import Dict, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.transforms import blended_transform_factory
 
 from wormlab3d import LOGS_PATH, START_TIMESTAMP
 from wormlab3d import logger
-from wormlab3d.data.model import Reconstruction
+from wormlab3d.data.model import Reconstruction, Eigenworms
 from wormlab3d.data.model.dataset import Dataset
 from wormlab3d.postures.eigenworms import generate_or_load_eigenworms
-from wormlab3d.toolkit.util import print_args
+from wormlab3d.toolkit.util import print_args, hash_data
 from wormlab3d.trajectories.cache import get_trajectory
 from wormlab3d.trajectories.util import calculate_speeds
+
+DATA_CACHE_PATH = LOGS_PATH / 'cache'
+os.makedirs(DATA_CACHE_PATH, exist_ok=True)
 
 show_plots = True
 save_plots = True
@@ -44,25 +49,17 @@ def parse_args() -> Namespace:
     return args
 
 
-def eigenworm_modulation(
-        by_concentration: bool = True,
-        by_reconstruction: bool = True,
-):
+def _calculate_data(
+        args: Namespace,
+        ds: Dataset,
+        ew: Eigenworms
+) -> Dict[float, Dict[str, np.ndarray]]:
     """
-    Show how eigenworms vary across concentrations/reconstructions.
+    Fetch the data.
     """
-    args = parse_args()
     logger.info('Fetching dataset.')
-    ds = Dataset.objects.get(id=args.dataset)
     metas = ds.metas
     lambdas = {}
-
-    ew = generate_or_load_eigenworms(
-        eigenworms_id=args.eigenworms,
-        dataset_id=args.dataset,
-        n_components=args.n_components,
-        regenerate=False
-    )
 
     # Group by concentration and then by reconstruction
     for i, rid in enumerate(metas['reconstruction']):
@@ -86,6 +83,75 @@ def eigenworm_modulation(
 
     # Sort by concentration
     lambdas = {k: v for k, v in sorted(list(lambdas.items()))}
+
+    return lambdas
+
+
+def _generate_or_load_data(
+        args: Namespace,
+        rebuild_cache: bool = False,
+        cache_only: bool = False
+) -> Tuple[Dataset, Eigenworms, Dict[float, Dict[str, np.ndarray]]]:
+    """
+    Generate or load the data.
+    """
+    logger.info('Fetching dataset.')
+    ds = Dataset.objects.get(id=args.dataset)
+    ew = generate_or_load_eigenworms(
+        eigenworms_id=args.eigenworms,
+        dataset_id=args.dataset,
+        n_components=args.n_components,
+        regenerate=False
+    )
+    additional = {
+        'min_speed': args.min_speed,
+        'max_speed': args.max_speed
+    }
+
+    cache_path = DATA_CACHE_PATH / f'ds={ds.id}_ew={ew.id}_{hash_data(additional)}'
+    cache_fn = cache_path.with_suffix(cache_path.suffix + '.npz')
+    lambdas = None
+    if not rebuild_cache and cache_fn.exists():
+        try:
+            data = np.load(cache_fn)
+            lambdas = {}
+            for name in data.files:
+                c, rid = name.split('_')
+                c = float(c)
+                if c not in lambdas:
+                    lambdas[c] = {}
+                lambdas[c][rid] = data[name]
+            lambdas = {k: v for k, v in sorted(list(lambdas.items()))}
+            logger.info(f'Loaded data from cache: {cache_fn}')
+        except Exception as e:
+            lambdas = None
+            logger.warning(f'Could not load cache: {e}')
+
+    if lambdas is None:
+        if cache_only:
+            raise RuntimeError(f'Cache "{cache_fn}" could not be loaded!')
+        logger.info('Generating data.')
+        lambdas = _calculate_data(args, ds, ew)
+        data = {
+            f'{c}_{rid}': r_vals
+            for c, c_vals in lambdas.items()
+            for rid, r_vals in c_vals.items()
+        }
+        logger.info(f'Saving data to {cache_fn}.')
+        np.savez(cache_path, **data)
+
+    return ds, ew, lambdas
+
+
+def eigenworm_modulation(
+        by_concentration: bool = True,
+        by_reconstruction: bool = True,
+):
+    """
+    Show how eigenworms vary across concentrations/reconstructions.
+    """
+    args = parse_args()
+    ds, ew, lambdas = _generate_or_load_data(args)
 
     # Determine positions
     concs = [float(k) for k in lambdas.keys()]
@@ -196,10 +262,111 @@ def eigenworm_modulation(
             plt.show()
 
 
+def eigenworm_modulation_tidy():
+    """
+    Show how eigenworms vary across concentrations/reconstructions.
+    Make a paper-ready bespoke plot.
+    """
+    args = parse_args()
+    ds, ew, lambdas = _generate_or_load_data(args, rebuild_cache=False, cache_only=False)
+    # exclude_concs = []
+    # break_at = 100
+    exclude_concs = [2.75, 4]
+    break_at = 5
+
+    # Determine positions
+    concs = [float(k) for k in lambdas.keys() if k not in exclude_concs]
+    ticks = np.arange(len(concs))
+
+    # Prepare output
+    out_conc = np.zeros((len(concs), ew.n_components))
+    out_reconst = {c: [] for c in concs}
+    out_reconst_stats = np.zeros((len(concs), ew.n_components, 2))
+    i = 0
+    for c, r_vals in lambdas.items():
+        if c in exclude_concs:
+            continue
+        lambdas_c = np.concatenate([v for v in r_vals.values()])
+        out_conc[i] = lambdas_c.sum(axis=0) / lambdas_c.sum()
+        out_reconst[c] = np.array([rv.sum(axis=0) / rv.sum() for rv in r_vals.values()])
+        out_reconst_stats[i] = np.array([out_reconst[c].mean(axis=0), out_reconst[c].std(axis=0)]).T
+        i += 1
+
+    # Make plots
+    cmap = plt.get_cmap('jet')
+    colours = cmap(np.linspace(0, 1, len(args.plot_components)))
+
+    fig, axes = plt.subplots(1, figsize=(8, 6), sharex=False, sharey=False)
+    ax = axes
+    ax.set_title('Eigenworm contributions by concentration')
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(concs)
+    ax.set_xlabel('Concentration')
+    ax.set_ylabel('Relative contribution')
+    # ax.axhline(y=0, linestyle=':', color='lightgrey', zorder=-1)
+
+    plot_args = dict(
+        linewidth=2,
+        marker='x',
+        markersize=10,
+        markeredgewidth=4,
+        alpha=0.8,
+    )
+
+    for i, idx in enumerate(args.plot_components):
+        ax.plot(
+            ticks[:break_at],
+            out_conc[:break_at, idx],
+            label=f'$\lambda_{i}$',
+            color=colours[i],
+            **plot_args
+        )
+        if break_at < len(ticks):
+            ax.plot(
+                ticks[break_at:],
+                out_conc[break_at:, idx],
+                color=colours[i],
+                **plot_args
+            )
+
+    if break_at < len(ticks):
+        dx = .04
+        dy = .025
+        dist = 0.06
+        trans = blended_transform_factory(ax.transData, ax.transAxes)
+        break_line_args = dict(transform=trans, color='k', clip_on=False)
+        x_div = break_at - 0.5
+        ax.plot((x_div - dist / 2 - dx, x_div - dist / 2 + dx), (-dy, dy), **break_line_args)
+        ax.plot((x_div + dist / 2 - dx, x_div + dist / 2 + dx), (-dy, dy), **break_line_args)
+        ax.plot((x_div - dist / 2 - dx, x_div - dist / 2 + dx), (1 - dy, 1 + dy), **break_line_args)
+        ax.plot((x_div + dist / 2 - dx, x_div + dist / 2 + dx), (1 - dy, 1 + dy), **break_line_args)
+        ax.axvline(x=x_div, linestyle=':', color='k')
+
+    ax.legend()
+    fig.tight_layout()
+
+    if save_plots:
+        path = LOGS_PATH / (f'{START_TIMESTAMP}'
+                            f'_ew_modulation_tidy'
+                            f'_ds={ds.id}'
+                            f'_ew={ew.id}'
+                            f'_plot={",".join([str(c_) for c_ in args.plot_components])}' +
+                            f'_exc={",".join([str(c_) for c_ in exclude_concs])}' +
+                            (f'_spmin={args.min_speed}' if args.min_speed is not None else '') +
+                            (f'_spmax={args.max_speed}' if args.max_speed is not None else '') +
+                            f'.{img_extension}')
+        logger.info(f'Saving plot to {path}.')
+        plt.savefig(path)
+
+    if show_plots:
+        plt.show()
+
+
 if __name__ == '__main__':
     if save_plots:
         os.makedirs(LOGS_PATH, exist_ok=True)
-    eigenworm_modulation(
-        by_concentration=True,
-        by_reconstruction=True,
-    )
+    # eigenworm_modulation(
+    #     by_concentration=True,
+    #     by_reconstruction=True,
+    # )
+    eigenworm_modulation_tidy()
